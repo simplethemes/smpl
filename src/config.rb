274d1bd5794a0ade6755fa93aee61ddeb05d72e4
Casey Lee <casey@simplethemes.com>
2014-09-16 14:40:17 -0500

    Updates to helpers

---
+++ src/config.rb
@@ -0,0 +1,10 @@
+# relative_assets = true
+http_path = "/"
+css_dir = "../css"
+output_style = :expanded
+# output_style = :expanded or :nested or :compact or :compressed
+line_comments = true
+sass_dir = "."
+images_dir = "../img"
+javascripts_dir = "../js"
+#require 'companimation'
